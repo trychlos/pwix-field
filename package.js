27e62ddd2db5083b778470f9905603cbacb12a48
Pierre Wieser <p.wieser@trychlos.org>
2024-06-24 09:44:49 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:field',
-    version: '1.1.0',
+    version: '1.1.1-rc',
     summary: 'Define common field specifications for Meteor usages',
     git: 'https://github.com/trychlos/pwix-field.git',
     documentation: 'README.md'
